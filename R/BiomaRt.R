9b48b4a334f06bb29e8df83e8848e945a140a380
sfragkoul <sfragkoul@certh.gr>
2022-01-25 14:40:09 +0200

    Vasilis script for BiomaRt.R

--- R/BiomaRt.R
+++ R/BiomaRt.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(biomaRt)
 
+rm(list = ls())
+
 #output_folder <- "C:/Users/vasil/Desktop/Phd/InterTADs-data/results_bloodcancer/"
 #expr_data <- 1
 #tech <- "hg19"
@@ -9,86 +11,86 @@ ensembl_ids <- function(output_folder = NULL,
                         expr_data = NULL,
                         tech = NULL) {
   data.all <- fread(paste(output_folder, "integrated-tad-table-methNorm.txt",
-    sep = ""
+                          sep = ""
   ),
   sep = "\t"
   )
-
+  
   expression <- data.all[which(data.all$parent == expr_data), ]
-
+  
   expression$names <- paste(expression$chromosome_name, expression$start_position, expression$end_position, sep = ":")
-
+  
   ensembl.expression <- list()
   
-
+  
   if (any(str_detect(expression$ID, "ENSG"))) {
     ensembl.expression[[1]] <- expression[which(str_detect(expression$ID, "ENSG")), ]
-
+    
     expression <- expression[which(!str_detect(expression$ID, "ENSG")), ]
   }
-
+  
   if (nrow(expression) > 0) {
     if (tech == "hg19") {
       ensembl <- useEnsembl(biomart = "ensembl", dataset = "hsapiens_gene_ensembl", GRCh = 37)
-
+      
       new_gene_ids <- getBM(
         attributes = c("chromosome_name", "start_position", "end_position", "ensembl_gene_id"),
         mart = ensembl
       )
-
+      
       new_gene_ids$names <- paste(new_gene_ids$chromosome_name, new_gene_ids$start_position, new_gene_ids$end_position, sep = ":")
-
-      who <- match(new_gene_ids$names, expression$names)
-      expression[who, ]$ID <- new_gene_ids$ensembl_gene_id
-
+      
+      who <- match(expression$names, new_gene_ids$names)
+      expression$ID <- new_gene_ids[who,]$ensembl_gene_id
+      
       expression <- expression[, -c("names")]
-
+      
       ensembl.expression[[2]] <- expression[which(str_detect(
         expression$ID,
         "ENSG"
       )), ]
-
+      
       expression <- expression[which(!str_detect(expression$ID, "ENSG")), ]
     } else {
       ensembl <- useEnsembl(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
-
+      
       new_gene_ids <- getBM(
         attributes = c("chromosome_name", "start_position", "end_position", "ensembl_gene_id"),
         mart = ensembl
       )
-
+      
       new_gene_ids$names <- paste(new_gene_ids$chromosome_name, new_gene_ids$start_position, new_gene_ids$end_position, sep = ":")
-
-      who <- match(new_gene_ids$names, expression$names)
-      expression[who, ]$ID <- new_gene_ids$ensembl_gene_id
-
+      
+      who <- match(expression$names, new_gene_ids$names)
+      expression$ID <- new_gene_ids[who,]$ensembl_gene_id
+      
       expression <- expression[, -c("names")]
-
+      
       ensembl.expression[[2]] <- expression[which(str_detect(
         expression$ID,
         "ENSG"
       )), ]
-
+      
       expression <- expression[which(!str_detect(expression$ID, "ENSG")), ]
     }
   }
-
+  
   ensembl.expression <- rbindlist(ensembl.expression)
-
+  
   other <- data.all[which(data.all$parent != expr_data), ]
-
-
+  
+  
   data.all <- rbind(expression, other, ensembl.expression)
-
+  
   write.table(data.all,
-    file = paste(output_folder,
-      "integrated-tad-table-methNorm-ensembl_1.txt",
-      sep = ""
-    ),
-    col.names = TRUE,
-    row.names = FALSE,
-    quote = FALSE,
-    sep = "\t"
+              file = paste(output_folder,
+                           "integrated-tad-table-methNorm-ensembl_1.txt",
+                           sep = ""
+              ),
+              col.names = TRUE,
+              row.names = FALSE,
+              quote = FALSE,
+              sep = "\t"
   )
   return(TRUE)
 }
