90e15ceb9866a286c421a1918f5f2ef678d71812
aspaor <aspasiaor@hotmail.com>
2022-03-17 12:19:12 +0200

    unit test data_integration

---
+++ tests/testthat/test_data_integration.R
@@ -0,0 +1,38 @@
+test_that('Test data integration runs properly',{
+  expect_vector( data_integration (
+
+        counts_folder = system.file(
+            "extdata", "Datasets", "counts", package = "InterTADs"
+        ),
+
+        freq_folder = system.file(
+            "extdata", "Datasets", "freq", package = "InterTADs"
+        ),
+
+        mapping_file = system.file(
+            "extdata", "Datasets", "meta-data.csv", package = "InterTADs"
+        ),
+
+        tad_file =system.file(
+            "extdata", "Datasets",
+            "hglft_genome_2dab_ec1330.bed", package = "InterTADs"
+        ),
+
+        tech = "hg19"
+    ))
+  
+  expect_error( data_integration (
+    
+    counts_folder = NULL,
+    
+    freq_folder = NULL,
+    
+    mapping_file =NULL,
+    
+    tad_file =NULL,
+    
+    tech =NULL
+  ))
+  
+  
+})
