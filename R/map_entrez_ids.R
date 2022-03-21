15a3b5b7a4b536d2b65a183d49a6de094cb8564a
unknown <nikosp41@gmail.com>
2022-03-21 12:57:41 +0200

    fix map_entrez_ids

--- R/map_entrez_ids.R
+++ R/map_entrez_ids.R
@@ -4,18 +4,25 @@
 #' @param entrez.ids
 #' @param tech
 #'
+#' @importFrom AnnotationDbi select
+#' @importFrom annotables grch37 grch38
+#' 
 #' @return
 #' @exports
 #'
 #' @examples
-map_entrez_ids <- function(entrez.ids, tech = "h19"){
+
+
+map_entrez_ids <- function(entrez.ids, tech = "hg19"){
 
     genes <- entrez.ids
 
-    mapping <- AnnotationDbi::select(org.Hs.eg.db,
-                                    key=genes,
-                                    columns=c("SYMBOL"),
-                                    keytype="ENTREZID")
+    mapping <- select(
+        org.Hs.eg.db,
+        key = genes,
+        columns = c("SYMBOL"),
+        keytype = "ENTREZID"
+    )
 
     mapping <- as.data.table(mapping)
     colnames(mapping) <- c("entrez.id", "hgnc.symbol")
@@ -24,11 +31,15 @@ map_entrez_ids <- function(entrez.ids, tech = "h19"){
     mapping <- mapping[which(!(is.na(mapping$hgnc.symbol))), ]
 
     if(length(not.found) > 0){
+        
         if(tech == "h19"){
+            
             x <- grch37
-        }
-        else {
+            
+        } else {
+            
             x <- grch38
+            
         }
 
         x <- as.data.table(x)
@@ -42,8 +53,14 @@ map_entrez_ids <- function(entrez.ids, tech = "h19"){
         not.found <- not.found[which(!(not.found %in% merge$entrez.id))]
       }
 
-    mapping <- rbind(mapping, data.table("entrez.id" = not.found,
-                                            "hgnc.symbol" = NA))
+    mapping <- rbind(
+        mapping, 
+        data.table(
+            "entrez.id" = not.found,
+            "hgnc.symbol" = NA
+        )
+    )
+    
     mapping <- mapping[order(mapping$hgnc.symbol), ]
 
     mapping <- unique(mapping)
